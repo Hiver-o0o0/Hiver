088416936e472fb913959807b784208724d15aa3
春风十里 <wangchunxi@finewe.com.cn>
2021-09-03 17:41:56 +0800

    feat: 配置修改

---
+++ docs/.vuepress/components/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div style="width: 100px;height: 100px;background-color: red;">
+    123</div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang='scss' scoped>
+</style>
